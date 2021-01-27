1f822f2ee9b0166f3292bea0ca10588d006cc60c
mikaelajoelle <74077372+mikaelajoelle@users.noreply.github.com>
2021-01-26 19:38:08 -0800

    removing unneeded import

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,7 +6,6 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Home from '../components/Home';
 import About from '../components/About';
-// import SingleMovie from '../components/SingleMovie';
 import Favourites from '../components/Favourites';
 import MoviePage from '../components/MoviePage';
 import PageNotFound from '../components/PageNotFound';
