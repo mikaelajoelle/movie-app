58255c70de34551e7e086f8f3aeaa4c5230bd136
mikaelajoelle <74077372+mikaelajoelle@users.noreply.github.com>
2021-01-26 19:36:09 -0800

    removing unused variables

--- src/globals/variables.js
+++ src/globals/variables.js
@@ -3,12 +3,6 @@
 const API_KEY_ONLY = 'b8996e057d12ef493274fb32a9fcb535';
 export const API_KEY = `?api_key=${API_KEY_ONLY}&language=en-US&page=1`;
 
-// export const API_DATA_MODE = '&mode=json';
-// export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
-
-// export const LANGUAGE = '&language=en-US&page=1&region=US';
-
-// export const DEFAULT_SORT_BY = 'popular';
 export const URL_POPULAR      = 'https://api.themoviedb.org/3/movie/popular';
 export const URL_TOP_RATED    = 'https://api.themoviedb.org/3/movie/top_rated';
 export const URL_UPCOMING     = 'https://api.themoviedb.org/3/movie/upcoming';
@@ -22,10 +16,6 @@ export const APP_FOLDER_NAME  = '/movie-app';
 export const URL_SEARCH = `https://api.themoviedb.org/3/search/movie${API_KEY}`
 
 
-// export const API_URL_REQUEST_GENRE =  `https://api.themoviedb.org/3/genre/movie/list${API_KEY}&language=en-US`;
-// export const API_URL_REQUEST_IMG = 'https://image.tmdb.org/t/p/w500/';
-
-// export const API_URL_REQUEST_SEARCH = `https://api.themoviedb.org/3/search/movie/${API_KEY}`;
-
+// Storage
 export const STORAGE_YOUR_MOVIES = 'your-fav-movies';
 
